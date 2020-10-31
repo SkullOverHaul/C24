ba3c6e1227a5416b190fe6e4b8ac9f866a36e4aa
SkullOverHaul <72302535+SkullOverHaul@users.noreply.github.com>
2020-10-31 12:09:32 +0530

    Add files via upload

---
+++ bird.js
@@ -0,0 +1,30 @@
+class Bird {
+    constructor(x, y) {
+      var options = {
+          'restitution':0.8,
+          'friction':1,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y,40,40, options);
+      this.width = 40;
+      this.height = 40;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      pos.y=mouseY
+      pos.x=mouseX
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      fill("red");
+      strokeWeight(4);
+      stroke("red")
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  };
+  
